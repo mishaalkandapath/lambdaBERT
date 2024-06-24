1e583bef1c4bdaa03cec8e3f676815a4869c642d
mishaalkandapath <mishaalhere@gmail.com>
2024-06-24 17:06:10 -0400

    token inference

--- error_analysis.py
+++ error_analysis.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from models import *
 import dataloader
 import tqdm
+from tokenization import get_bert_emb, TOKENIZER
 
 
 #model inference
@@ -43,6 +44,47 @@ def plot_confusion_matrix(confusion_matrix):
     plt.yticks(list(label_map.keys()), list(label_map.values()))
     plt.colorbar()
     plt.savefig("confusion_matrix.png")
+
+def get_discrete_output(input_sent, model):
+    #bert tokenize and get embeddings
+    tokenized = TOKENIZER(input_sent, return_tensors="pt", padding=True)
+    input_embs = get_bert_emb(tokenized)
+    input_embs = input_embs.to(DEVICE)
+    #model inference
+    out, classified_class, var_reg = model(input_embs)
+    classified_class = classified_class.argmax(dim=-1).squeeze(0)
+    out = out.squeeze(0)
+    var_reg = var_reg.squeeze(0)
+    out = out.argmax(dim=-1)[]
+    out.argmax(dim=-1)[classified_class] = 0
+    out_list = TOKENIZER.convert_ids_to_tokens(out.tolist())
+    
+    lambda_indices, app_indices, var_indices  = torch.where(classified_class == 2), torch.where(classified_class == 3), torch.where(classified_class == 1)
+    for i in lambda_indices:
+        out_list[i] = "λ"
+    for i in app_indices:
+        out_list[i] = "("
+    
+    # time for variables
+    var_dict = {}
+    for i in var_indices:
+        if len(var_dict) == 0:
+            var_dict[0] = var_reg[i]
+            out_list[i] = f"x{len(var_dict)}"
+        else:
+            min_sim = 0
+            min_key = 0
+            for key in var_dict:
+                sim = torch.cosine_similarity(var_reg[i], var_dict[key])
+                if sim > min_sim:
+                    min_sim = sim
+                    min_key = key
+            if min_sim > 0.9:
+                out_list[i] = f"x{min_key}"
+            else:
+                var_dict[len(var_dict)] = var_reg[i]
+                out_list[i] = f"x{len(var_dict)}"
+    return " ".join(out_list)
 
 if __name__ == "__main__":
     #arguments
